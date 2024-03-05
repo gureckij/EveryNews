ce5c18c442dbd4b9d05cacc3fb12a615720334ea
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-05 16:11:00 +0300

    (6/11) Сделайте кнопку "Тёмная" неактивной

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ let darkThemeButton = document.querySelector('.theme-button-dark');
 let lightThtmeButton = document.querySelector('.theme-button-light');
 
 document.body.classList.remove('dark');
+
+darkThemeButton.classList.remove('active');
