5b23b33f4b7b22ab45aa24f4e3aee989384a039e
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-05 16:14:51 +0300

    (6/11) Сделайте кнопку "Светлая" активной

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let darkThemeButton = document.querySelector('.theme-button-dark');
 let lightThtmeButton = document.querySelector('.theme-button-light');
 
+// код переключения цвета темы
 document.body.classList.remove('dark');
-
 darkThemeButton.classList.remove('active');
+lightThtmeButton.classList.add('active');
