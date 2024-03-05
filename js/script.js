9c9be1d31ba52152ff021a79abe258ec79dd8dd9
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-05 16:19:51 +0300

    (7/11) Сохранить в переменную кнопку без засечек

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let darkThemeButton = document.querySelector('.theme-button-dark');
 let lightThtmeButton = document.querySelector('.theme-button-light');
 let serifFontButton = document.querySelector('.font-button-serif');
+let sansSerifFontButton = document.querySelector('.font-button-sans-serif');
 
 // код переключения цвета темы
 document.body.classList.remove('dark');
