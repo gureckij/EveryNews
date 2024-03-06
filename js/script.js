a36345015030f1c75198bb91918545b17a7b8790
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-06 17:22:32 +0300

    (8/11) Добавьте обработчик события клика кнопки lightThemeButton

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let lightThtmeButton = document.querySelector('.theme-button-light');
 let serifFontButton = document.querySelector('.font-button-serif');
 let sansSerifFontButton = document.querySelector('.font-button-sans-serif');
 
+lightThtmeButton.onclick = function () {};
+
 // код переключения цвета темы
 document.body.classList.remove('dark');
 darkThemeButton.classList.remove('active');
