9977d66135ab2043ab9aad53a5a37dce8783c7b9
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-06 17:24:16 +0300

    (8/11) Перенесите в функцию-обработчик код переключения на светлую тему

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ let lightThtmeButton = document.querySelector('.theme-button-light');
 let serifFontButton = document.querySelector('.font-button-serif');
 let sansSerifFontButton = document.querySelector('.font-button-sans-serif');
 
-lightThtmeButton.onclick = function () {};
-
-// код переключения цвета темы
-document.body.classList.remove('dark');
-darkThemeButton.classList.remove('active');
-lightThtmeButton.classList.add('active');
+lightThtmeButton.onclick = function () {
+    // код переключения цвета темы
+    document.body.classList.remove('dark');
+    darkThemeButton.classList.remove('active');
+    lightThtmeButton.classList.add('active');
+};
 
 // код переключения шрифта
 document.body.classList.remove('serif');
