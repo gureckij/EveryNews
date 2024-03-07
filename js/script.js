b9091de53d9cebe014700a85a8bd3e21458daaa6
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-07 14:42:37 +0300

    (5/11) Сохраните коллекцию элементов .main-article-section в переменной articleSection

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let articleSection = document.querySelector('.main-article-section');
+let articleSections = document.querySelectorAll('.main-article-section');
 let moreButton = articleSection.querySelector('.more');
+
 moreButton.onclick = function () {
     articleSection.classList.remove('short');
 };
