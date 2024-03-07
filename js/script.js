6ce639f6176292946021164c12892fa7884e84f2
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-07 15:11:47 +0300

    (6/7) Улалите поиск и сохранение первого элемента .main-article-section из кода

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-let articleSection = document.querySelector('.main-article-section');
 let articleSections = document.querySelectorAll('.main-article-section');
 
 for (let articleSection of articleSections) {
