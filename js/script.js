1994a387d63278d5f9ea08a01b52b6c35e5b3ef9
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-07 14:29:01 +0300

    (4/7) Добавьте обработчик события onclick элементу moreButton

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 let articleSection = document.querySelector('.main-article-section');
 let moreButton = articleSection.querySelector('.more');
+moreButton.onclick = function () {};
 articleSection.classList.remove('short');
